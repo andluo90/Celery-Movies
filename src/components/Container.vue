38c8c6f33f461ce34d3c20ced1acc26681e3f516
andluo <1455234749@qq.com>
2019-07-03 17:54:06 +0800

    分享Loading组件

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -6,19 +6,20 @@
       </div>
       
     </section>
+    <Loading v-if="isLoading"></Loading>
     
-    <div class="loading" v-if="isLoading">
-        <span class="iconfont icon-Loading"></span>
-    </div>
     
   </div>
 </template>
 
 <script>
 import {mapState} from 'vuex'
+import Loading from './Loading'
 export default {
   name: 'Container',
-  
+  components:{
+    Loading
+  },
   computed:{
     ...mapState(['isLoading'])
   }
@@ -30,42 +31,14 @@ export default {
     .Container {
         position: relative;
 
-        @keyframes rotate {
-            0% {
-                transform: rotate(0deg);
-            }
-            100% {
-                transform:rotate(360deg);
-            }
-        }
-
-        > .loading {
-            position: absolute;
-            top: calc(50vh - 50px);
-            left: 50%;
-            transform: translateX(-50%);  
-
-            .iconfont {
-              display: inline-block;
-              font-size: 3em;
-              color:lightpink;
-              animation: 1s rotate linear infinite;
-            }
-        }
-
-
         #test {
           height: 2000px;
         }
 
         >section {
-          
           overflow:scroll;
           -webkit-overflow-scrolling: touch;
-
         }
-        
-        
 
     }
 </style>
---
+++ src/components/Loading.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="loading" >
+        <span class="iconfont icon-Loading"></span>
+    </div>
+</template>
+<script>
+export default {
+    name:"Loading"
+}
+</script>
+<style lang="scss" scoped>
+
+    @keyframes rotate {
+        0% {
+            transform: rotate(0deg);
+        }
+        100% {
+            transform:rotate(360deg);
+        }
+    }
+
+    .loading {
+        position: absolute;
+        top: calc(50vh - 50px);
+        left: 50%;
+        transform: translateX(-50%);  
+
+        .iconfont {
+            display: inline-block;
+            font-size: 3em;
+            color:lightpink;
+            animation: 1s rotate linear infinite;
+        }
+    }
+</style>
+
