1d222ec808573f13f5d143bd3a46e31a65ffa4a7
andluo <1455234749@qq.com>
2019-07-05 22:28:57 +0800

    图片懒加载

--- src/components/MovieItem.vue
+++ src/components/MovieItem.vue
@@ -2,7 +2,9 @@
     <div class="item" :key="movie.id" @click='showDetail' >
         <a >
             <div class="cover">
-                <img src="http://img3.doubanio.com/f/movie/b6dc761f5e4cf04032faa969826986efbecd54bb/pics/movie/movie_default_small.png" :data-src = "movie.images.small" alt="">
+                <!-- <img src="http://img3.doubanio.com/f/movie/b6dc761f5e4cf04032faa969826986efbecd54bb/pics/movie/movie_default_small.png" :data-src = "movie.images.small" alt=""> -->
+                <img v-lazy="movie.images.small" alt="">
+
             </div>
             <div class="detail">
                 <h2>{{ movie.title }}</h2>
--- src/main.js
+++ src/main.js
@@ -6,11 +6,15 @@ import router from './router'
 import { log } from 'util';
 import store from './store'
 import axios from 'axios'
+import VueLazyload from 'vue-lazyload'
 
 Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
-
+Vue.use(VueLazyload,{
+  loading:"http://img3.doubanio.com/f/movie/b6dc761f5e4cf04032faa969826986efbecd54bb/pics/movie/movie_default_small.png",
+  error:"http://img3.doubanio.com/f/movie/b6dc761f5e4cf04032faa969826986efbecd54bb/pics/movie/movie_default_small.png"
+})
 
 /* eslint-disable no-new */
 new Vue({
