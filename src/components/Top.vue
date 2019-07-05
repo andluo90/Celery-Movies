acfc2240872b911c5b734c52f9bb2c37092dd021
andluo <1455234749@qq.com>
2019-07-05 23:42:51 +0800

    初步完成加载更多功能

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="top" :class="classes">
         <MovieList :data="movieList"></MovieList>
+        <div class="more" @click="loadMore">点击加载更多</div>
     </div>
     
 </template>
@@ -18,7 +19,7 @@ export default {
           apikey:this.$store.state.apikey,
           start:0,
           count:20,
-          movieList:null
+          movieList:[]
 
       }
     },
@@ -27,32 +28,53 @@ export default {
             return this.$store.state.isShowDetail ? 'hide':''
         }
     },
-    beforeMount(){
-        this.$store.commit('setLoading',{status:true})
-        jsonp(`//api.douban.com/v2/movie/top250?apikey=${this.apikey}&start=${this.start}&count=${this.count}`,
-                null,
-                (error,data)=>{
-                    this.$store.commit('setLoading',{status:false})
-                    if(error){
-                        console.log("请求失败.");
-                        console.log(error);
-                    }else{
-                        console.log("请求成功.");
-                        this.movieList = data.subjects
-                        this.start += 20
+    methods:{
+        loadMore:function(){
+            console.log('加载更多...')
+            jsonp(`//api.douban.com/v2/movie/top250?apikey=${this.apikey}&start=${this.start}&count=${this.count}`,
+                    null,
+                    (error,data)=>{
+                        this.$store.commit('setLoading',{status:false})
+                        if(error){
+                            console.log("请求失败.");
+                            console.log(error);
+                        }else{
+                            console.log("请求成功.");
+                            const temp = JSON.stringify(this.movieList.concat(data.subjects))
+                            this.movieList = JSON.parse(temp)
+                            this.start += 20
+                        }
                     }
-                }
-            )
+                )
+        },
+        getData:function(){
+            this.$store.commit('setLoading',{status:true})
+            jsonp(`//api.douban.com/v2/movie/top250?apikey=${this.apikey}&start=${this.start}&count=${this.count}`,
+                    null,
+                    (error,data)=>{
+                        this.$store.commit('setLoading',{status:false})
+                        if(error){
+                            console.log("请求失败.");
+                            console.log(error);
+                        }else{
+                            console.log("请求成功.");
+                            this.movieList = data.subjects
+                            this.start += 20
+                        }
+                    }
+                )
+        }
+    },
+    beforeMount(){
         
+        this.getData()
         
     },
 
     mounted(){
         this.$store.commit('toggleTab',{tab:"Top"})
-    },
-    destroyed(){
-        console.log(`Top is destroyed...`)
     }
+    
     
 }
 </script>
@@ -64,6 +86,14 @@ export default {
         &.hide {
             position: absolute;
             left: -140%;
+        }
+        >.more {
+            font-size: 13px;
+            margin-bottom: 50px;
+            padding-bottom: 10px;
+            text-align: center;
+            padding-top: 20px;
+            color: #999;
         }
     }
 
